5dea258a8a08ac8f47ce35c34508695158b159a2
dadaa <daisuke@mozilla-japan.org>
2014-01-14 00:23:51 +0900

    update

--- data/getStatus.js
+++ data/getStatus.js
@@ -2,7 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var statuses = document.querySelectorAll('.full_bug_ids li');
-for (var i = 0; i < statuses.length; i++) {
-  self.postMessage(statuses[i].textContent+':'+self.options.errorId);
+var relatedBugs = document.querySelectorAll(".bug-link-with-data");
+var message = document.title;
+for (var i = 0; i < relatedBugs.length; i++) {
+  var bug = relatedBugs[i]
+  var id = bug.getAttribute("data-id");
+  var status = bug.getAttribute("data-status");
+  var resolution = bug.getAttribute("data-resolution");
+  resolution = resolution.length == 0 ? "---" : resolution;
+  var summary = bug.getAttribute("data-summary");
+  message += "\n";
+  message += id+" "+status+" "+resolution+" "+summary;
 }
+self.postMessage(message);
