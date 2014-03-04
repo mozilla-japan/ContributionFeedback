032c10f06619640a707b1e393156ac937a2d1965
dadaa <daisuke@mozilla-japan.org>
2014-03-04 15:50:42 +0900

    change the UI

--- data/script4bugpage.js
+++ data/script4bugpage.js
@@ -2,12 +2,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var users = document.querySelectorAll(".bz_comment_user");
 var message = "";
-if (users.length > 0) {
-  var user = users[users.length-1];
+
+var ths = document.querySelectorAll("#bz_show_bug_column_2 .field_label");
+var indexOfLastModified = -1;
+for (var i = 0, n = ths.length; i < n; i++) {
+  var th = ths[i];
+  if (th.textContent.toLowerCase().indexOf("modified:") > -1) {
+  	indexOfLastModified = i;
+	break;
+  }
+}
+var tds = document.querySelectorAll("#bz_show_bug_column_2 td");
+var targetLastModified = tds[indexOfLastModified].textContent;
+var regexLastModified = /^\s*(.+)\D\D\D\n/;
+var resultLastModified = regexLastModified.exec(targetLastModified);
+var lastModified = resultLastModified[1];
+message += lastModified;
+
+var users = document.querySelectorAll(".bz_comment_user");
+for (var i = 0, n = users.length; i < n; i++) {
+  var user = users[i];
   var icon = user.querySelector("img");
   var vcard = user.querySelector(".vcard");
-  message = icon.getAttribute("src")+" "+vcard.textContent;
+  message += "\n";
+  message += icon.getAttribute("src")+" "+vcard.textContent;
 }
 self.postMessage(message);
