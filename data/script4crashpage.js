d9e93fbdd4ba1057f842d4af4339967dbacdc663
dadaa <daisuke@mozilla-japan.org>
2014-03-06 16:39:25 +0900

    fixed #8

--- data/script4crashpage.js
+++ data/script4crashpage.js
@@ -2,16 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var relatedBugs = document.querySelectorAll(".bug-link-with-data");
-var message = document.title;
-for (var i = 0; i < relatedBugs.length; i++) {
-  var bug = relatedBugs[i]
-  var id = bug.getAttribute("data-id");
-  var status = bug.getAttribute("data-status");
-  var resolution = bug.getAttribute("data-resolution");
-  resolution = resolution == null || resolution.length == 0 ? "---" : resolution;
-  var summary = bug.getAttribute("data-summary");
-  message += "\n";
-  message += id+" "+status+" "+resolution+" "+summary;
+
+var pageHeading = document.querySelectorAll(".page-heading");
+var notfoundORarchive = pageHeading.length == 0;
+if (notfoundORarchive == true) {
+  self.postMessage("");
+} else {
+  var message = document.title;
+  var relatedBugs = document.querySelectorAll(".bug-link-with-data");
+  for (var i = 0; i < relatedBugs.length; i++) {
+  	var bug = relatedBugs[i]
+  	var id = bug.getAttribute("data-id");
+  	var status = bug.getAttribute("data-status");
+  	var resolution = bug.getAttribute("data-resolution");
+  	resolution = resolution == null || resolution.length == 0 ? "---" : resolution;
+  	var summary = bug.getAttribute("data-summary");
+  	message += "\n";
+  	message += id+" "+status+" "+resolution+" "+summary;
+  }
+  self.postMessage(message);
 }
-self.postMessage(message);
